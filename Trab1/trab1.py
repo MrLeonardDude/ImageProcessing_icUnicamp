4796ea3ae209561842d4be001b0ecfe271d1e52a
MrLeonardDude <leopagnez@gmail.com>
2018-03-21 13:37:23 -0300

    Test #2 : contour and RGB2Grey

--- Trab1/trab1.py
+++ Trab1/trab1.py
@@ -1,6 +1,7 @@
-import skimage
+from skimage import measure
 import numpy as np
 from scipy import misc
+from skimage import color
 import matplotlib.pyplot as plt
 
 #Pede-se o input da imagem que sera avaliada
@@ -8,12 +9,13 @@ img_name = input('Digite o nome da imagem:')
 
 # Utilizando misc e pyplot, a imagem eh aberta
 img = misc.imread(img_name + '.png')
+grey_img = color.rgb2grey(img)
 
-contours = measure.find_contours(img, 0.8)
+contours = measure.find_contours(grey_img, 0.8)
 
 # Display the image and plot all contours found
 fig, ax = plt.subplots()
-ax.imshow(r, interpolation='nearest', cmap=plt.cm.gray)
+ax.imshow(grey_img, interpolation='nearest', cmap=plt.cm.gray)
 
 for n, contour in enumerate(contours):
     ax.plot(contour[:, 1], contour[:, 0], linewidth=2)
