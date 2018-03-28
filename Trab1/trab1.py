3372ed311617a2818bb92444bd3372a44a2e73d7
MrLeonardDude <leopagnez@gmail.com>
2018-03-28 14:50:25 -0300

    Delivery Version with some Results

--- Trab1/trab1.py
+++ Trab1/trab1.py
@@ -8,7 +8,7 @@ from skimage.measure import label, regionprops
 #histograma - Item 1.4
 def drawHistogram(img, img_name):
     a = np.asarray(img).reshape(-1)
-    plt.hist(a, bins=[0,1500,3000,5000])
+    plt.hist(a, bins=[0,1500,3000,5000], rwidth=0.5)
     plt.savefig(img_name + 'histograma.png')
     plt.xlabel('Area')
     plt.title('histograma de area dos objetos')
@@ -17,22 +17,19 @@ def drawHistogram(img, img_name):
 
 #Pede-se o input da imagem que sera avaliada
 img_name = input('Digite o nome da imagem:')
-
 img = misc.imread(img_name + '.png')
+
+#Transformacao de Cores - Item 1.1
 grey_img = color.rgb2grey(img)
 img_flip = util.invert(grey_img)
 misc.imsave(img_name + 'Cinza.png', grey_img)
-
-#Transformacao de Cores - Item 1.1
 plt.imshow(grey_img, cmap='gray')
 plt.show()
 
-
-fig, ax = plt.subplots()
-label_img, num = label(img_flip, None, None, True, None)
-regions = regionprops(label_img)
+#------------------------------------------------------------------------------
 
 #Contorno dos Objetos - Item 1.2
+fig, ax = plt.subplots()
 contours = measure.find_contours(grey_img, 0.8)
 ax.imshow(grey_img, interpolation='nearest', cmap=plt.cm.gray)
 
@@ -44,10 +41,11 @@ ax.set_xticks([])
 ax.set_yticks([])
 plt.show()
 fig.savefig(img_name + 'Contorno.png')
-
-
+#------------------------------------------------------------------------------
 
 #Propriedades do Objeto - Item 1.3
+label_img, num = label(img_flip, None, None, True, None)
+regions = regionprops(label_img)
 smallArea = 0
 mediumArea = 0
 bigArea = 0
@@ -65,7 +63,7 @@ for n, reg in enumerate(regions):
 
 print()
 
-#Rotulando Imagens
+
 fig2, ax2 = plt.subplots()
 ax2.imshow(img_flip, cmap='gray')
 for n, region in enumerate(regions):
@@ -74,10 +72,13 @@ for n, region in enumerate(regions):
 plt.show()
 fig2.savefig(img_name + 'Rotulado.png')
 
-print('Item 1.4 - Classificao dos Objetos')
+#------------------------------------------------------------------------------
+
 # Histograma de  Area dos Objetos - 1.4 - Classificao dos Objetos
+print('Item 1.4 - Classificao dos Objetos')
 print('numero de regioes pequenas: ', smallArea)
 print('numero de regioes medias: ', mediumArea)
 print('numero de regioes grandes: ', bigArea)
-#Item 1.4
 drawHistogram(regArray, img_name)
+
+#------------------------------------------------------------------------------
